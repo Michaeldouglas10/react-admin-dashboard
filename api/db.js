001f4134ecf7bb31f63b05ea0716f71c2d7c8752
Michael Douglas <69938299+Michaeldouglas10@users.noreply.github.com>
2024-08-13 15:23:57 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
+    host:"bukli8qaivb1qmjaorfa-mysql.services.clever-cloud.com",
+    user:"uagwibkw4qwbo0ur",
+    password:"l7QRU6m9Q61gSxhSmuue",
     port: "3306",
-    database:"banco"
+    database:"bukli8qaivb1qmjaorfa"
 })
